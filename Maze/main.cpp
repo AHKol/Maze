6a880643f789946dc24f55df6d1b66e8d7e708ba
AHKol <adam.kolodko@hotmail.com>
2017-08-27 19:57:35 -0400

    Added Node and NodeControl Classes, Some kind of memory leak to fix

--- Maze/main.cpp
+++ Maze/main.cpp
@@ -1,11 +1,47 @@
-//Main + Tester
-//TODO Constructor
+#include <vector>
 #include "Maze.h"
+//Main + Tester
+//Node class and node control
+using namespace std;
+class node {
+private:
+   bool m_isWall;
+   int m_xAxis;
+   int m_yAxis;
+public:
+   node(){}
+   ~node() {}
+   node(bool isSpace, int x, int y) {
+      
+   }
+};
+class nodeContol {
+private:
+   vector<vector<node>> m_nodeArray;
+public:
+   nodeContol() {}
+   ~nodeContol() {}
+   nodeContol(Maze mazeInput) {
+      for (int i = 0; i < mazeInput.height(); i++) {
+         for (int j = 0; j < mazeInput.width(); j++) {
+            m_nodeArray[i].push_back(node(mazeInput.mazeArray()[i][j], i, j));
+         }
+      }
+   }
+};
+class path {
+
+};
+class intersection {
+
+};
+
+
 
 int main() {
 
    Maze mazeA("Maze.bmp");
    mazeA.print();
-
+   nodeContol nc(mazeA);
    return 0;
 }
